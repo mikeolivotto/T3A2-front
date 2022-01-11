d809828e2712590c0396b12a4ae4c21b7a571f7e
NicholasSaltis <gcas012107@coderacademy.edu.au>
2022-01-11 15:32:31 +1100

    added log out button and event handler to remove profile and Id token from state

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,24 @@
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useGlobalState } from "../config/store";
 
 function Nav() {
+  const {store,dispatch} = useGlobalState()
+  const handleLogOut = (event) => {
+    event.preventDefault()
+    dispatch({type: "removeProfile"})
+  }
   return (
     <nav>
       <ul>
         <li>
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/profile">Profile</Link>
-        </li>
       </ul>
+      {
+        (store.idToken) ? <Button onClick={handleLogOut}>Log Out</Button> : null
+      }
+      
     </nav>
   );
 }
