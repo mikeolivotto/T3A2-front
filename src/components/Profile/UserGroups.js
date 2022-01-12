611d8a412e739bc6bf0277963f746a8997af2381
mikeolivotto <michaelolivotto@gmail.com>
2022-01-12 13:42:26 +1100

    Render groups in profile

--- src/components/Profile/UserGroups.js
+++ src/components/Profile/UserGroups.js
@@ -5,26 +5,26 @@ function UserGroups() {
 
   const { store } = useGlobalState()
 
-  const groupArray = store.profileData[2]
+   const groupArray = store.profileData[2].concat(store.profileData[3])
 
 
   const groupsList = groupArray.map((group) => {
-    return <ListGroup.Item key={group.id}>{ group }</ListGroup.Item>
+    return <ListGroup.Item key={ group._id }>{ group.groupName }</ListGroup.Item>
   })
 
 
   const groups = () => {
     if(groupArray.length >= 1) {
-      return <ListGroup className="col-8 col-md-5 col-lg-3 mx-auto">{groupsList}</ListGroup>
+      return <ListGroup className="col-8 col-md-5 col-lg-3 mx-auto">{ groupsList }</ListGroup>
     } else {
       return <p>You have not joined any groups, amigo</p>
     }
   }
 
     return (
-      <>
+      <div style={{color: "blue", maxHeight: "20vh", overflowY: "scroll"}}>
       { groups() }
-      </>
+      </div>
     );
   }
   
